22209c207cfb9157cfdf967b4bc397a5fa407995
szenadam <szenadam1@gmail.com>
2020-07-21 13:25:00 +0200

    Add ManifestChecker

---
+++ ManifestTester.Tests/ManifestCheckerTestcs.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+using Xunit;
+
+namespace ManifestTester.Tests
+{
+    public class ManifestCheckerTest
+    {
+        [Fact]
+        public void CheckRootElement()
+        {
+            XmlDocument doc = new XmlDocument();
+            var checker = new ManifestChecker();
+            checker.Check(doc);
+            Assert.False(checker.Result.Passed);
+            
+            XmlNode node = doc.CreateElement("manifest");
+            doc.AppendChild(node);
+            checker.Check(doc);
+            Assert.False(checker.Result.Passed);
+            
+            
+            doc.RemoveAll();
+            node = doc.CreateElement("manifest", "http://www.imsglobal.org/xsd/imscp_v1p1");
+            doc.AppendChild(node);
+            checker.Check(doc);
+            Assert.True(checker.Result.Passed);
+        }
+    }
+}
---
+++ ManifestTester/ManifestChecker.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml;
+
+namespace ManifestTester
+{
+    public class ManifestChecker
+    {
+        public Result Result { get; }
+
+        public ManifestChecker()
+        {
+            Result = new Result();
+        }
+
+        public void Check(XmlDocument doc)
+        {
+            var root = doc.DocumentElement;
+            if (root != null && 
+                root.Name == "manifest" &&
+                root.NamespaceURI == "http://www.imsglobal.org/xsd/imscp_v1p1")
+            {
+                Result.Passed = true;
+            }
+            else
+            {
+                Result.Passed = false;
+            }
+        }
+    }
+    public class Result
+    {
+        public bool Passed { get; set; }
+    }
+}

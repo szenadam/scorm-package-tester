d7cc26e76c6a43e5cf439dbe32e6d894cec59b8f
szenadam <szenadam1@gmail.com>
2020-07-22 15:14:00 +0200

    Refactor ManifestChecker
    Set result to private. Return Result on check()

--- ManifestTester.Tests/ManifestCheckerTestcs.cs
+++ ManifestTester.Tests/ManifestCheckerTestcs.cs
@@ -5,25 +5,25 @@ namespace ManifestTester.Tests
 {
     public class ManifestCheckerTest
     {
-        [Fact]
-        public void CheckRootElement()
+        [Fact(DisplayName = "Should pass on valid root element and namespace")]
+        public void Check_RootElement_PassAndFail()
         {
             XmlDocument doc = new XmlDocument();
             var checker = new ManifestChecker();
-            checker.Check(doc);
-            Assert.False(checker.Result.Passed);
+            Result result = checker.Check(doc);
+            Assert.False(result.Passed);
             
             XmlNode node = doc.CreateElement("manifest");
             doc.AppendChild(node);
-            checker.Check(doc);
-            Assert.False(checker.Result.Passed);
+            result = checker.Check(doc);
+            Assert.False(result.Passed);
             
             
             doc.RemoveAll();
             node = doc.CreateElement("manifest", "http://www.imsglobal.org/xsd/imscp_v1p1");
             doc.AppendChild(node);
-            checker.Check(doc);
-            Assert.True(checker.Result.Passed);
+            result = checker.Check(doc);
+            Assert.True(result.Passed);
         }
     }
 }
--- ManifestTester/ManifestChecker.cs
+++ ManifestTester/ManifestChecker.cs
@@ -4,26 +4,28 @@ namespace ManifestTester
 {
     public class ManifestChecker
     {
-        public Result Result { get; }
+        private readonly Result _result;
 
         public ManifestChecker()
         {
-            Result = new Result();
+            _result = new Result();
         }
 
-        public void Check(XmlDocument doc)
+        public Result Check(XmlDocument doc)
         {
-            var root = doc.DocumentElement;
+            XmlElement root = doc.DocumentElement;
             if (root != null && 
                 root.Name == "manifest" &&
                 root.NamespaceURI == "http://www.imsglobal.org/xsd/imscp_v1p1")
             {
-                Result.Passed = true;
+                _result.Passed = true;
             }
             else
             {
-                Result.Passed = false;
+                _result.Passed = false;
             }
+
+            return _result;
         }
     }
 }
